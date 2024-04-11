caec428aa88167c0130bebe6b0e4cd1917aa8e5b
Edison <aalevenegass@gmail.com>
2024-04-11 13:57:29 -0400

    Importando file system de node

--- backend_app/index.js
+++ backend_app/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import { writeFile, readFile } from "node:fs/promises";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/canciones", (req, res) => {
   res.json("hola mundo");
